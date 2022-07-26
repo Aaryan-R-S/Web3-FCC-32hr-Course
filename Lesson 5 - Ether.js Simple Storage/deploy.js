90bc1bd09c5d821ae3460d6580218c9e7a695e39
Aaryan-R-S <aaryan20004@iiitd.ac.in>
2022-07-26 19:26:34 +0530

    L5 Added yarn

---
+++ Lesson 5 - Ether.js Simple Storage/deploy.js
@@ -0,0 +1,12 @@
+async function main() {
+    // http://127.0.0.1:8545
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
